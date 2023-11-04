0ce6a07d945d23cfde44ec0e68670d56d8654d43
ANDREW NGUYEN <andrew.nguyen.ta@gmail.com>
2023-11-04 18:40:51 -0500

    added image

--- frontend/lib/pages/home.dart
+++ frontend/lib/pages/home.dart
@@ -1,1 +1,25 @@
 import 'package:flutter/material.dart';
+
+class Test_Name extends StatelessWidget {
+  const Test_Name({Key? home}) : super(key: home);
+
+  @override
+  Widget build(BuildContext context) {
+    return Test_Name(
+        home: Scaffold(
+            body: SingleChildScrollView(
+              child: Center(
+                child: Container(
+                  width: 200,
+                  height: 200,
+                  decoration: BoxDecoration(
+                    color: Colors.blue,
+                    borderRadius: BorderRadius.circular(12.0),
+                  ),
+                )
+              )
+            )
+        )
+    );
+  }
+}
