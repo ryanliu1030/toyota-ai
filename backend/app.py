3735c995c9d9c80522582e0d11120c2989fe740b
Noel <77630469+DescendingMisery@users.noreply.github.com>
2023-11-04 16:06:42 -0500

    add backend folder

---
+++ backend/app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+import google.generativeai as palm
+API_KEY = 'AIzaSyDmZnz_tzgyL6GotLqDdSsKYWymhn0c1v4'
+
+palm.configure(api_key=API_KEY)
+    
+#response = palm.chat(messages='Hello')
+#response.last
+#response = response.reply("Just chillin'")
+#print(response.last)
+
+app = Flask(__name__)
+@app.route('/')
+def index():
+    return "Hello world"
+if __name__ == "__main__":
+    app.run(debug=True)
+
