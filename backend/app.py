16add74464db4e910e310ec16019e74341feb008
Noel <77630469+DescendingMisery@users.noreply.github.com>
2023-11-04 17:00:50 -0500

    Co-authored-by: ryanliu1030 <ryanliu1030@users.noreply.github.com>

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 import google.generativeai as palm
 API_KEY = 'AIzaSyDmZnz_tzgyL6GotLqDdSsKYWymhn0c1v4'
 
@@ -9,10 +9,33 @@ palm.configure(api_key=API_KEY)
 #response = response.reply("Just chillin'")
 #print(response.last)
 
+
 app = Flask(__name__)
 @app.route('/')
 def index():
     return "Hello world"
+
+# test data
+vehicle_status ={
+    'id':1,
+    'make':'Toyota',
+    'model':'Supra',
+    'year': '2023',
+    'status':'parking'
+}
+@app.route('/car/status', methods =['GET', 'POST'])
+
+def vehicle_status():
+    if request.method =='GET':
+        return jsonify(vehicle_status)
+    elif request.method == 'POST':
+        data = request.get_json()
+        if 'status' in data:
+            car_status['status'] = data['status']
+            return jsonify({'message': 'Car status updated successfully'})
+        else:
+            return jsonify({'error': 'Status field is required in the request data'}), 400
+
 if __name__ == "__main__":
     app.run(debug=True)
 
